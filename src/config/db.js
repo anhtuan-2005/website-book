62b1f4f45d6e5620344d00402429fd3f2445a3c7
Nguyen Anh Tuan <tn7193154@gmail.com>
2026-05-02 00:32:53 +0700

    config db ssl

--- src/config/db.js
+++ src/config/db.js
@@ -7,9 +7,15 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    // TiDB Cloud sử dụng cổng 4000 thay vì 3306 mặc định
+    port: process.env.DB_PORT || 4000, 
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    // Bắt buộc phải có SSL để kết nối với Database trên Cloud như TiDB
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool.promise();
